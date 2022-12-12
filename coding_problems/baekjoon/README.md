1e9c80ea7b3f75c70a24c6891f14fcdc95221cbd
Chan <seulchan07@gmail.com>
2022-12-12 22:58:32 +0900

    upload 백준 11650 solution

--- coding_problems/baekjoon/README.md
+++ coding_problems/baekjoon/README.md
@@ -1,3 +1,5 @@
 # Baekjoon Coding Problems
 
 ## Problems
+### Sorting
+- [좌표 정렬하기](solutions/baek_11650.py)
---
+++ coding_problems/baekjoon/solutions/baek_11650.py
@@ -0,0 +1,43 @@
+import sys
+from queue import PriorityQueue
+
+
+class Pair:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __lt__(self, other):
+        if self.x == other.x:
+            return self.y < other.y
+        else:
+            return self.x < other.x
+
+
+## 53448 KB / 840 ms
+def pq_version():
+    num = int(sys.stdin.readline())
+    que = PriorityQueue()
+
+    for i in range(num):
+        a, b = map(int, sys.stdin.readline().split())
+        que.put(Pair(a, b))
+
+    for i in range(num):
+        pair = que.get()
+        print(pair.x, pair.y)
+
+
+# 	50064 KB / 364 ms
+def list_comprehension_version():
+    num = int(sys.stdin.readline())
+
+    pos = [list(map(int, sys.stdin.readline().split())) for i in range(num)]
+    pos.sort()
+
+    for i in pos:
+        print(*i)
+
+
+list_comprehension_version()
