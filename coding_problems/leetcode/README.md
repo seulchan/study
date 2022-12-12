fa21fd9f024662b00015f603c7b46a4cae8a6242
Chan <seulchan07@gmail.com>
2022-12-12 11:04:51 +0900

    upload leet 509 solution

--- coding_problems/leetcode/README.md
+++ coding_problems/leetcode/README.md
@@ -3,3 +3,4 @@
 ## Problems
 - [Reverse Linked List](solutions/leet_206.py)
 - [Merge Two Sorted Lists](solutions/leet_21.py)
+- [Fibonacci Number](solutions/leet_509.py)
---
+++ coding_problems/leetcode/solutions/leet_509.py
@@ -0,0 +1,9 @@
+def fib(n: int) -> int:
+    if n <= 1:
+        return n
+
+    return fib(n - 1) + fib(n - 2)
+
+if __name__ == "__main__":
+    n = 4
+    print(fib(n))
