07b0f55b3889c88fa577eeb254e318ef1a372ac3
Chan <seulchan07@gmail.com>
2022-12-13 22:51:33 +0900

    upload leet 20 solution

--- coding_problems/leetcode/README.md
+++ coding_problems/leetcode/README.md
@@ -1,12 +1,16 @@
 # Leetcode Coding Problems
 
 ## Problems
-### Binary Search
-- [Binary Search](solutions/leet_704.py)
-
 ### Linked List
 - [Reverse Linked List](solutions/leet_206.py)
 - [Merge Two Sorted Lists](solutions/leet_21.py)
 
+### Stack
+- [Valid Parentheses](solutions/leet_20.py)
+
+### Binary Search
+- [Binary Search](solutions/leet_704.py)
+
 ### Recursive
 - [Fibonacci Number](solutions/leet_509.py)
+- 
---
+++ coding_problems/leetcode/solutions/leet_20.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/valid-parentheses/
+
+def is_valid(s: str) -> bool:
+    stack = []
+    close_to_open = {")" : "(", "]" : "[", "}": "{"}
+    for c in s:
+        if c in close_to_open:
+            if stack and stack[-1] == close_to_open[c]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(c)
+
+    return True if not stack else False
+
+
+if __name__ == "__main__":
+    s = "()[]{}"
+    print(is_valid(s))
+
+    s = "([]{}"
+    print(is_valid(s))
