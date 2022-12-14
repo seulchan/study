6fdb1bbe1571139fcf779f5d5c974799f187bd6a
Chan <seulchan07@gmail.com>
2022-12-14 17:16:36 +0900

    update binary search on some range

--- algorithms/search/implementation/binary_search.py
+++ algorithms/search/implementation/binary_search.py
@@ -17,6 +17,32 @@ def binary_search(nums: List[int], target: int) -> int:
 
     return -1
 
+
+# low = 1, high = 100
+# Binary search on some range of values
+def binary_search_on_range(low, high):
+    while low <= high:
+        middle = (low + high) // 2
+
+        if is_correct(middle) > 0:
+            high = middle - 1
+        elif is_correct(middle) < 0:
+            low = middle + 1
+        else:
+            return middle
+    return -1
+
+
+# Return 1 if n is too big, -1 if too small, 0 if correct
+def is_correct(n):
+    if n > 10:
+        return 1
+    elif n < 10:
+        return -1
+    else:
+        return 0
+
+
 if __name__ == "__main__":
     target = 9
     data_list = random.sample(range(20), 10)
