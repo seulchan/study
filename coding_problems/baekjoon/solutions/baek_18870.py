592e830017400c96cb8e35037fd3b2509343dd5a
Chan <seulchan07@gmail.com>
2022-12-15 17:34:08 +0900

    use dict comprehension

--- coding_problems/baekjoon/solutions/baek_18870.py
+++ coding_problems/baekjoon/solutions/baek_18870.py
@@ -4,9 +4,7 @@ if __name__ == "__main__":
     num = int(sys.stdin.readline())
     x_axis_list = list(map(int, sys.stdin.readline().split()))
 
-    index_map = {}
-    for index, value in enumerate(sorted(set(x_axis_list))):
-        index_map[value] = index
+    index_map = {value: index for index, value in enumerate(sorted(set(x_axis_list)))}
 
     for x in x_axis_list:
         print(index_map.get(x), end=" ")
